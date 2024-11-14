8c6625cb327bc7d323e7bd38c9ca48c720aa1b46
Mauricio <lmauricio-2010@hotmail.com>
2024-11-13 21:40:48 -0300

    feat: style input and output buttons

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from '@radix-ui/react-dialog';
+
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
   width: 100vw;
@@ -7,6 +8,7 @@ export const Overlay = styled(Dialog.Overlay)`
   inset: 0;
   background: rgba(0, 0, 0, 0.75);
 `;
+
 export const Content = styled(Dialog.Content)`
   min-width: 32rem;
   border-radius: 6px;
@@ -48,6 +50,7 @@ export const Content = styled(Dialog.Content)`
     }
   }
 `;
+
 export const CloseButton = styled(Dialog.Close)`
   position: absolute;
   background: transparent;
@@ -57,4 +60,31 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   cursor: pointer;
   color: ${props => props.theme["gray-500"]};
+`;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${props => props.theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${props => props.theme["gray-300"]};
+  svg {
+    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+  }
 `;
